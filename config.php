b8bb1780f0bb037502a488adf72e101ea774c462
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-15 15:45:36 +0100

    modif accès connection bdd

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('PASSWORD', '');
 define('HOST', 'localhost');
 define('DATABASE', 'test');
 try {
-    $connection = new PDO("mysql:host=localhost;dbname=projet_trans_reseau_b2", 'root', '');
+    $connection = new PDO("mysql:host=localhost;dbname=projet_trans_reseau_b2", 'root', 'rempart');
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
 }
