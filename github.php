5b532a57f8f17093016b638b5d3cb6705d5ba110
NoYouCanNot <64852203+NoYouCanNot@users.noreply.github.com>
2021-03-15 16:17:50 +0100

    Update github.php

--- github.php
+++ github.php
@@ -5,7 +5,7 @@ if ($_POST && $_POST['payload']) {
     if (isset($payload['ref']) && $payload['ref'] == 'refs/heads/master') {
 //         exec("rm /var/www/git-stuward/webhook.log");
         exec("cd /var/www/html/ && rm -R projet_trans_b2/* && rm -R projet_trans_b2/.git");
-        exec("cd /var/www/html/ && git clone ssh://git@github.com/Alexandre-stoppini/projet_trans_b2.git && echo $(date) ' : Success' >> ./webhook.log || echo $(date)' : Error' >> ./webhook.log");
+        exec("cd /var/www/html/ && git clone git clone https://github.com/Alexandre-stoppini/projet_trans_b2.git && echo $(date) ' : Success' >> ./webhook.log || echo $(date)' : Error' >> ./webhook.log");
         exec("tail -1 /var/www/html/projet_trans_b2/webhook.log | wall --nobanner $1");
     }
 }
