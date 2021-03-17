bf90ae3da2a85c94045a015640c214ac607cf96b
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-17 15:01:39 +0100

    correction

--- registration.php
+++ registration.php
@@ -44,11 +44,11 @@ if (isset($_POST['register'])) {
         if ($result) {
             try {
 
-                shell_exec("/opt/scripts/test.sh");
-//                shell_exec("newuser.sh " + $username + " " + $password);
-//
-//                ChromePhp::log('Test de la commande shell : "newuser.sh " + $username + " " + $password');
-//                ChromePhp::log(shell_exec("newuser.sh " + $username + " " + $password));
+//                shell_exec("/opt/scripts/test.sh");
+                shell_exec("newuser.sh " + $username + " " + $password);
+
+                ChromePhp::log('Test de la commande shell : "newuser.sh " + $username + " " + $password');
+                ChromePhp::log(shell_exec("newuser.sh " + $username + " " + $password));
 
                 echo '<p class="success">Your registration was successful!</p>';
             } catch (exception $e) {
