47870df254574f0aa1a088602a9b7a5d092579cc
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-23 15:31:27 +0100

    divers tests

--- nav_file.php
+++ nav_file.php
@@ -6,7 +6,7 @@ include 'ChromePhp.php';
 <div class="vide">
 
     <?php
-    $result = shell_exec("ls /home/rempart/");
+    $result = shell_exec("cat /home/rempart/test.txt");
     ChromePhp::log($result);
     echo $result;
     ?>
