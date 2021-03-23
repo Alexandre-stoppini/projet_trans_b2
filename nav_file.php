16a0eb33118da574df3ea127de22dc97361c0439
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-23 15:29:10 +0100

    divers tests

--- nav_file.php
+++ nav_file.php
@@ -1,11 +1,13 @@
 <link rel="stylesheet" href="assets/css/style.css">
 <?php
 include_once('includes/front/header.php');
+include 'ChromePhp.php';
 ?>
 <div class="vide">
 
     <?php
     $result = shell_exec("ls /home/rempart/");
+    ChromePhp::log($result);
     echo $result;
     ?>
 
