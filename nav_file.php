5d5ee490acf6e450868dbc8530aac9311072dd85
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-23 15:16:03 +0100

    Création de la page nav_file.php

---
+++ nav_file.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="assets/css/style.css">
+<?php
+include_once ('includes/front/header.php');
+
+?>
+<div class="vide">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<?php
+include_once ('includes/front/footer.php');
+?>
