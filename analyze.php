7a8d41c682e3a54edcee38150ebfb7c79c6999b7
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-31 12:48:16 +0200

    modif

---
+++ analyze.php
@@ -0,0 +1,14 @@
+<?php
+include_once("includes/front/header.php");
+?>
+
+<p>Analyse des données serveurs :</p>
+<ul>
+    <li>A faire : espace disque occupée par les sauvegardes</li>
+    <li>A faire : date de la dernière sauvegarde</li>
+</ul><?php
+include_once("includes/front/footer.php");
+?>
+
+</body>
+</html>
