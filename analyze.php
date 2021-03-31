ecd6fdbe6abd242934cfd75301624632e257c857
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-03-31 13:05:55 +0200

    modif

--- analyze.php
+++ analyze.php
@@ -1,22 +1,22 @@
 <?php
 include_once("includes/front/header.php");
-
-
+include 'ChromePhp.php';
 if (empty($_SESSION['username'])) {
     ?>
     <meta http-equiv="refresh" content="1; URL=index.php">
     <p>Vous n'êtes pas autorisé à accéder à cette page.</p>
     <?php
 } else {
-$size_file = shell_exec("du -h " . $_SESSION["username"] );
-echo $size_file;
+    $size_file = shell_exec("du -h " . $_SESSION["username"]);
+    ChromePhp::log("Essaie du script du -h.");
+    ChromePhp::log($size_file);
     ?>
 
-<p>Analyse des données serveurs :</p>
-<ul>
-    <li>A faire : espace disque occupée par les sauvegardes</li>
-    <li>A faire : date de la dernière sauvegarde</li>
-</ul><?php
+    <p>Analyse des données serveurs :</p>
+    <ul>
+        <li>A faire : espace disque occupée par les sauvegardes</li>
+        <li>A faire : date de la dernière sauvegarde</li>
+    </ul><?php
 
 
 }
