d6232eef0edbcb8d69e4f19102b73c22b6551e8b
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-02 22:43:48 +0200

    ajout du début de la fonction récursive

--- analyze.php
+++ analyze.php
@@ -11,7 +11,7 @@ if (empty($_SESSION['username'])) {
 //    $commande_size = 'du -sh "/sauvegarde/' . $_SESSION["username"] . '/rempart" '/*| tail -1 | cut -d "/" -f1'*/;
 
     $commande_size = 'sudo du -sh /sauvegarde '/*| tail -1 | cut -d "/" -f1'*/;
-    $size_file = exec("$commande_size");
+    $size_file = shell_exec("$commande_size");
 
     $commande_last_modif = "stat -c %y /sauvegarde/" . $_SESSION["username"] . " | cut -d '.' -f1";
     $last_modif = exec("$commande_last_modif");
