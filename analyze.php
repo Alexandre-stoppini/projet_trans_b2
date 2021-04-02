ed5e8ec299d971048f3ce0f6c246b635e4768c61
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-02 21:29:02 +0200

    ajout du début de la fonction récursive

--- analyze.php
+++ analyze.php
@@ -12,9 +12,9 @@ if (empty($_SESSION['username'])) {
     //'du -h | grep \''.$_SESSION["username"].'\' | tail -1 | cut -d "." -f1'
     // fonctionne : ls /var/sauvegarde/devatom
     // pour les tests : 'ls /var/test/devatom'
-   $commande_last_modif = "stat -c %y /sauvegarde/devatom | cut -d '.' -f1";
+   $commande_last_modif = 'stat -c %y /sauvegarde/\''. $_SESSION["username"] .'\' | cut -d '.' -f1';
    $last_modif = shell_exec("$commande_last_modif");
-   $commande_size = 'du -sh "/sauvegarde/devatom" | tail -1 | cut -d "/" -f1';
+   $commande_size = 'du -sh "/sauvegarde/\''. $_SESSION["username"] .'\'" | tail -1 | cut -d "/" -f1';
    $size_file = shell_exec("$commande_size");
 //
 //    ChromePhp::log('Essaie du script du -h | grep \'' .$_SESSION["username"].'\' | tail -1 | cut -d "." -f1');
