99dd2b4f1b4f209f5ba617a4c70a292904828917
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-02 12:37:00 +0200

    modif

--- nav_file.php
+++ nav_file.php
@@ -12,7 +12,7 @@ if (empty($_SESSION['username'])) {
     ChromePhp::log($test);
     ChromePhp::log(gettype($test));
     echo $test;
-    $test_array = preg_split("[^ ]*", $test);
+    $test_array = preg_split("[^\s]*", $test);
     ChromePhp::log("Tableau de test : ".$test_array[0]." et ".$test_array[1]);
     ?>
 
