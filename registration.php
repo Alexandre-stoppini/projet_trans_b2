ece8a155e131ff60a1a58b90f2069fa23f0d112a
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-02 11:50:29 +0200

    Ajout de path et distri dans le formulaire de registration

--- registration.php
+++ registration.php
@@ -13,8 +13,12 @@ if (isset($_POST['register'])) {
     $password = $_POST['password'];
     $password_rep = $_POST['password_rep'];
     $ip = $_POST['ip'];
+    $path = $_POST['path'];
+    $distri = $_POST['distri'];
     $error = 0;
     $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+
     $verifMail = $connection->prepare("SELECT * FROM users WHERE email=:email");
     $verifMail->bindParam("email", $email, PDO::PARAM_STR);
     $verifMail->execute();
@@ -22,6 +26,8 @@ if (isset($_POST['register'])) {
         echo '<p class="error">The email address is already registered!</p>';
         $error++;
     }
+
+
     $verifUser = $connection->prepare("SELECT * FROM users WHERE username=:username");
     $verifUser->bindParam("username", $username, PDO::PARAM_STR);
     $verifUser->execute();
@@ -29,6 +35,8 @@ if (isset($_POST['register'])) {
         echo '<p class="error">The username is already registered!</p>';
         $error++;
     }
+
+
     $verifIP = $connection->prepare("SELECT * FROM users where ip=:ip");
     $verifIP->bindParam("ip", $ip, PDO::PARAM_STR);
     $verifIP->execute();
@@ -36,12 +44,14 @@ if (isset($_POST['register'])) {
         echo '<p class="error">Error with the IP contact an administrator.</p>';
         $error++;
     }
+
+
     if ($_POST['password_rep'] != $_POST['password']) {
         echo '<p class="error">Passwords mismatch</p>';
         $error++;
     }
     if ($error == 0) {
-        $query = $connection->prepare("INSERT INTO users(username, address, company, email, telephone ,password, ip) VALUES (:username, :address, :company, :email, :telephone,:password_hash, :ip)");
+        $query = $connection->prepare("INSERT INTO users(username, address, company, email, telephone ,password, ip, path, distri) VALUES (:username, :address, :company, :email, :telephone,:password_hash, :ip, :path, :distri)");
         $query->bindParam("username", $username, PDO::PARAM_STR);
         $query->bindParam("address", $address, PDO::PARAM_STR);
         $query->bindParam("company", $company, PDO::PARAM_STR);
@@ -49,6 +59,8 @@ if (isset($_POST['register'])) {
         $query->bindParam("telephone", $telephone, PDO::PARAM_INT);
         $query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
         $query->bindParam("ip", $ip, PDO::PARAM_STR);
+        $query->bindParam("path", $path, PDO::PARAM_STR);
+        $query->bindParam("distri", $distri, PDO::PARAM_STR);
         $result = $query->execute();
         if ($result) {
             try {
@@ -75,6 +87,9 @@ if (isset($_POST['register'])) {
         }
     }
 }
+
+// path du fichier à sauvegarder
+// type de dsitri
 ?><main>
 <form method="post" action="" name="signup-form">
     <div class="form-element">
@@ -104,7 +119,7 @@ if (isset($_POST['register'])) {
 
     <div class="form-element">
         <label>Ip</label>
-        <input type="ip" name="ip" required>
+        <input type="text" name="ip" required>
     </div>
 
     <div class="form-element">
@@ -115,6 +130,19 @@ if (isset($_POST['register'])) {
     <div class="form-element">
         <label>Confirm Password</label>
         <input type="password" name="password_rep" required/>
+    </div>
+
+    <div class="form-element">
+        <label>Enter the path of the file you want to save</label>
+        <input type="text" name="path" required/>
+    </div>
+
+    <div class="form-element">
+        <label for="distri">Choose your distribution : </label>
+        <select id="distri" name="distri">
+            <option value="windows">Windows</option>
+            <option value="unix">Unix</option>
+        </select>
     </div>
 
     <button type="submit" name="register" value="register">Register</button>
