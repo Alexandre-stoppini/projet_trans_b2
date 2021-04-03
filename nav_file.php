4df925993638836adf1764aaf4f87bf5b03bb5ce
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-03 11:43:32 +0200

    ajout du début de la fonction récursive

--- nav_file.php
+++ nav_file.php
@@ -2,12 +2,12 @@
 include_once('includes/front/header.php');
 include 'ChromePhp.php';
 session_start();
-if (empty($_SESSION['username'])) {
-    ?>
-    <meta http-equiv="refresh" content="1; URL=index.php">
-    <p>Vous n'êtes pas autorisé à accéder à cette page.</p>
-    <?php
-} else {
+//if (empty($_SESSION['username'])) {
+//    ?>
+<!--    <meta http-equiv="refresh" content="1; URL=index.php">-->
+<!--    <p>Vous n'êtes pas autorisé à accéder à cette page.</p>-->
+<!--    --><?php
+//} else {
     $test = shell_exec('ls /var/sauvegarde');
     ChromePhp::log($test);
     ChromePhp::log(gettype($test));
@@ -20,7 +20,7 @@ if (empty($_SESSION['username'])) {
     ?>
 
 
-    <a href="/home/devatom/sauvegarde/rempart/test.txt" id="dl_serv" download>Test de dl</a>
+    <a href="/home/devatom/sauvegarde/rempart" id="dl_serv" download>Test de dl</a>
     <a href="/bdd/acces_bdd.txt" id="dl_serv" download>Test de dl</a>
 
 
@@ -29,4 +29,4 @@ if (empty($_SESSION['username'])) {
 //    $result = shell_exec('cat /home/rempart/test.txt');
 //    ChromePhp::log($result);
 //    echo $result;
-}
+//}
