e0e9170869c91145f22d0a601457b1f7d90495ec
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-03 13:07:52 +0200

    ajout du début de la fonction récursive

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 include_once("includes/front/header.php");
 include 'ChromePhp.php';
-ChromePhp::log(shell_exec("/opt/scripts/newuser.sh ant ant 192.168.10.2 /home"));
-ChromePhp::log("TEST");
 
-$test = shell_exec("/opt/scripts/newuser.sh ant ant 192.168.10.2 /home");
-echo $test;
+
 ?>
 
     <p>poueteuh le retour</p>
