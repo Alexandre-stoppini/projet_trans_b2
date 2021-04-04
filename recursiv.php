fe7f991e7756dd3c827f562e0d6ed4a3dc543802
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 12:26:45 +0200

    ajout du début de la fonction récursive

--- recursiv.php
+++ recursiv.php
@@ -22,12 +22,13 @@ function explore($path)
 //    }
     $chemin = shell_exec("tree -J $path");
     $chemin_array_file = preg_split('/({"type":")|(","name":")|("})|(,)|(","contents":\[)|(\[)|(]})|(report","directories":\d*,"files":\d*)/', $chemin);
+    var_dump($chemin_array_file);
     for ($i = 0; $i < count($chemin_array_file); $i++) {
      if ($i % 2 == 0) {
             if ($chemin_array_file[$i] == "directory") {
                 echo "<span><br>Dossier : </span>"/* . $chemin_array_file[$i]*/;
             } elseif ($chemin_array_file[$i] == "file") {
-                echo "<span>Fichier : </span> " /*. $chemin_array_file[$i]*/;
+                echo "<span><br>Fichier : </span> " /*. $chemin_array_file[$i]*/;
             }
         } elseif ($i % 2 == 1){
             echo  $chemin_array_file[$i];
