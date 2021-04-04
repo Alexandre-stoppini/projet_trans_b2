d6a6e278311be5100fe36745ac0cfb8ecce9c831
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 10:49:48 +0200

    ajout du début de la fonction récursive

--- recursiv.php
+++ recursiv.php
@@ -20,45 +20,43 @@ function explore($path)
 //            echo $chemin_array[$i];
 //        }
 //    }
-//    $chemin = shell_exec("tree $path");
-//    echo $chemin;
-    /*
+
+    $chemin = shell_exec("tree -J $path");
+    echo $chemin;
     $chemin_array = preg_split("/[{]+/", $chemin);
     for ($i = 1; $i < count($chemin_array); $i++) {
         echo $chemin_array[$i];
-    }*/
-
-        $result = array();
-        $cdir = scandir($path);
-        foreach ($cdir as $key => $value) {
-            if (!in_array($value, array(".", ".."))) {
-                if (is_dir($path . DIRECTORY_SEPARATOR . $value)) {
-                    $result[$value] = explore($path . DIRECTORY_SEPARATOR . $value);
-                } else {
-                    $result[] = $value;
-                }
-            }
-        }
-        return $result;
-
-
-
+    }
+//        $result = array();
+//        $cdir = scandir($path);
+//        foreach ($cdir as $key => $value) {
+//            if (!in_array($value, array(".", ".."))) {
+//                if (is_dir($path . DIRECTORY_SEPARATOR . $value)) {
+//                    $result[$value] = explore($path . DIRECTORY_SEPARATOR . $value);
+//                } else {
+//                    $result[] = $value;
+//                }
+//            }
+//        }
+//        return $result;
 }
-function scanDirectories($rootDir, $allData=array()) {
+
+function scanDirectories($rootDir, $allData = array())
+{
     // set filenames invisible if you want
     $invisibleFileNames = array(".", "..", ".htaccess", ".htpasswd");
     // run through content of root directory
     $dirContent = scandir($rootDir);
-    foreach($dirContent as $key => $content) {
+    foreach ($dirContent as $key => $content) {
         // filter all files not accessible
-        $path = $rootDir.'/'.$content;
-        if(!in_array($content, $invisibleFileNames)) {
+        $path = $rootDir . '/' . $content;
+        if (!in_array($content, $invisibleFileNames)) {
             // if content is file & readable, add to array
-            if(is_file($path) && is_readable($path)) {
+            if (is_file($path) && is_readable($path)) {
                 // save file name with path
                 $allData[] = $path;
                 // if content is a directory and readable, add path and name
-            }elseif(is_dir($path) && is_readable($path)) {
+            } elseif (is_dir($path) && is_readable($path)) {
                 // recursive callback to open new directory
                 $allData = scanDirectories($path, $allData);
             }
