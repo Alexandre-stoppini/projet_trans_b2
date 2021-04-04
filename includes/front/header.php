57ec57804c38837345d001f0f5305454e1169800
antoine-13 <fouvryantoine@gmail.com>
2021-04-04 18:29:58 +0200

    css

--- includes/front/header.php
+++ includes/front/header.php
@@ -23,11 +23,7 @@ session_start();
             <?php
         } else {
             ?>
-            <div>
-            <?php
-                echo 'Bienvenue ' . $_SESSION["username"];
-            ?>
-            </div>
+            
             
             <a href="analyze.php">Analyse de l'espace serveur</a>
             <a href="nav_file.php">Explorateur de fichiers</a>
