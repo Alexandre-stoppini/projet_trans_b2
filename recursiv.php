e0520a2ae9cc0b5c188692048f4d4cfa0f46f2f1
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 13:15:21 +0200

    modif branche antoine

--- recursiv.php
+++ recursiv.php
@@ -17,9 +17,13 @@ function explore($path)
 //      }
     $chemin_array = preg_split('/(\/sauvegarde\/)|(\s->\s)/', $chemin);
     for ($i = 0; $i < count($chemin_array); $i++) {
+
+        // print tous les fichiers
         if (preg_match('/(\.)/', $chemin_array[$i])) {
             $foo = preg_split('/(\/)/',$chemin_array[$i]);
-            echo $foo[count($foo)-1];
+            echo "<span><br>Votre fichier à télécharger : </span>" . $foo[count($foo)-1];
+        } else {
+            echo "<span><br>" . $chemin_array[$i]."</span>";
         }
     }
 }
