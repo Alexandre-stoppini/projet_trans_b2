0f02d27ec7ae1b80dfdaf3f2d3429aa2f2aac48c
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 10:51:33 +0200

    ajout du début de la fonction récursive

--- nav_file.php
+++ nav_file.php
@@ -19,6 +19,7 @@ if (empty($_SESSION['username'])) {
 //
 //        echo "<p>" . $test_array[$i] ."</p>";
 //  }
+    echo "test";
    explore("/var/www/html/projet_trans_b2");
 
     //var_dump(scanDirectories("/var/www/html/projet_trans_b2"));
