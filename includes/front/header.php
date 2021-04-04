d90226973be23f22dfbcb6f1262fcc163b82e201
antoine-13 <fouvryantoine@gmail.com>
2021-04-04 18:53:10 +0200

    css

--- includes/front/header.php
+++ includes/front/header.php
@@ -29,6 +29,19 @@ session_start();
                 <a href="logout.php">Logout</a>
             </div>
             <div class="account">
+                <div>
+                    <svg  
+                        width="30" 
+                        height="30"
+                        stroke="black"
+                        stroke-width="30"
+                        fill="none">
+
+                    <circle cx="300" cy="300" r="265" />
+                    <circle cx="300" cy="230" r="115" />	
+                    <path d="M106.81863443903,481.4 a205,205 1 0,1 386.36273112194,0" stroke-linecap="butt" />
+                    </svg>
+                </div>
                 <p>
             <?php
                 echo 'Bienvenue ' . $_SESSION["username"];
