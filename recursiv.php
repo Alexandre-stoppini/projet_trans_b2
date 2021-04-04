be5cdadb8ce0ec3a68afdb69ed41aba2c8acc2be
antoine-13 <fouvryantoine@gmail.com>
2021-04-04 16:13:32 +0200

    css

--- recursiv.php
+++ recursiv.php
@@ -10,13 +10,13 @@ function explore($path)
         // print tous les fichiers
         if (preg_match('/(\.)/', $chemin_array[$i])) {
             $foo = preg_split('/(\/)/',$chemin_array[$i]);
-            echo "<div class='file'><span><br>Votre fichier à télécharger : </span>" . $foo[count($foo)-1] . "<a href='/sauvegarde/". $chemin_array[$i] ."' download>
+            echo "<div class='file'><span>Votre fichier à télécharger : </span>" . $foo[count($foo)-1] . "<a href='/sauvegarde/". $chemin_array[$i] ."' download>
             <svg>
             <path d='M17 12v5H3v-5H1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5z'/>
             <path d='M10 15l5-6h-4V1H9v8H5l5 6z'/>
             </svg></a></div>";
         } else {
-            echo "<div class='dir' ><span><br>" . $chemin_array[$i]."</span></div>";
+            echo "<div class='dir' ><span>" . $chemin_array[$i]."</span></div>";
         }
     }
 }
