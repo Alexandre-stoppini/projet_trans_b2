ff97316c2d0acf43984597864b8224701410ede7
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 15:23:34 +0200

    Modif

--- recursiv.php
+++ recursiv.php
@@ -3,24 +3,14 @@ function explore($path)
 {
 
     $chemin = shell_exec("tree -fi $path");
-    //tree -J
-//    $chemin_array_file = preg_split('/({"type":")|(","name":")|("})|(,)|(","contents":\[)|(\[)|(]})|(report","directories":\d*,"files":\d*(})|(]))/', $chemin);
-//    for ($i = 0; $i < count($chemin_array_file); $i++) {
-//        if ($chemin_array_file[$i] == "directory") {
-//            echo "<span><br>Dossier : </span>";
-//        } elseif ($chemin_array_file[$i] == "file") {
-//            echo "<span><br>Fichier : </span>";
-//        } else {
-//            echo $chemin_array_file[$i];
-//        }
-//      }
+
     $chemin_array = preg_split('/(\/sauvegarde\/)|(\d*\sdirectories,\s\d*\sfiles)/', $chemin);
     for ($i = 0; $i < count($chemin_array); $i++) {
 
         // print tous les fichiers
         if (preg_match('/(\.)/', $chemin_array[$i])) {
             $foo = preg_split('/(\/)/',$chemin_array[$i]);
-            echo "<span><br>Votre fichier à télécharger : </span>" . $foo[count($foo)-1];
+            echo "<span><br>Votre fichier à télécharger : </span>" . $foo[count($foo)-1] . "<a href='/sauvegarde/". $chemin_array ." download'>Télécharger</a>";
         } else {
             echo "<span><br>" . $chemin_array[$i]."</span>";
         }
