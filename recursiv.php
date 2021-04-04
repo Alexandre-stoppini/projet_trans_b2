ae9ec81e9aa9c4799640405b1dc80150f6a949de
Alexandre-stoppini <53072955+Alexandre-stoppini@users.noreply.github.com>
2021-04-04 12:35:33 +0200

    modif branche antoine

--- recursiv.php
+++ recursiv.php
@@ -7,8 +7,14 @@ function explore($path)
     $chemin_array_file = preg_split('/({"type":")|(","name":")|("})|(,)|(","contents":\[)|(\[)|(]})|(report","directories":\d*,"files":\d*)/', $chemin);
     //var_dump($chemin_array_file);
     for ($i = 0; $i < count($chemin_array_file); $i++) {
-       // if ($chemin_array_file)
-        //     if ($i % 2 == 0) {
+        if ($chemin_array_file[$i] == "directory") {
+            echo "<p>Dossier : </p>";
+        } elseif ($chemin_array_file[$i] == "file") {
+            echo "<p>Fichier : </p>";
+        } else {
+            echo $chemin_array_file[$i];
+        }
+            //     if ($i % 2 == 0) {
 //            if ($chemin_array_file[$i] == "directory") {
 //                echo "<span><br>Dossier : </span>"/* . $chemin_array_file[$i]*/;
 //            } elseif ($chemin_array_file[$i] == "file") {
